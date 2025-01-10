29061c5b4d8c1dfe03a4474addeb2c3f9783f08a
Tomșa Alexandru-Eduard <tomsaalex@gmail.com>
2025-01-10 22:58:09 +0200

    Fixed file structure issues that caused Tailwind classes to not work

--- app.go
+++ app.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 )
 
 // App struct
@@ -19,9 +18,7 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
-}
-
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+	/*
+		hwnd := win.FindWindow(nil, syscall.StringToUTF16Ptr("BitTorrent_Client"))
+		win.SetWindowLong(hwnd, win.GWL_EXSTYLE, win.GetWindowLong(hwnd, win.GWL_EXSTYLE)|win.WS_EX_LAYERED)*/
 }
